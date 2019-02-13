1d80c8a7bec5e24037106790497656ea4f323424
jeremybeauchemin <jbeaucjemin2701@gmail.com>
2019-02-13 17:54:31 -0500

    second commit

--- Challenge/Challenge/Program.cs
+++ Challenge/Challenge/Program.cs
@@ -1,21 +1,91 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Challenge
+namespace challenge
 {
-    class Program
+    public class square
     {
+        
+        public static int Height = 20;
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.Write("Enter Height :");
+            Height = int.Parse(Console.ReadLine());
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.Write(" Method 1: " + "\n Method 2: " + "\n Method 3: " + "\n LEAVE 4: ");
+            var no = Console.ReadLine();
+            var leave = 4;
+            if (no == "1")
+            {
+                Method1();
+            }
+            else if (no == "2")
+            {
+                Method2();
+            }
+            else if (no == "3")
+            {
+                Method3();
+            }
+            else if (no == "leave")
+            {
+                Environment.Exit(4);
+            }
+            else
+            {
+                Console.WriteLine("you can jsute enter number dumbass");
+            }
+            Console.WriteLine();
+        }
+        private static void Method1()
+        {
+
+            for (int i = 0; i <= Height; i++)
+            {
+                for (int l = 0; l <= Height; l++)
+                {
+                    if (i == 0 || i == Height || l == 0 || l == Height)
+
+                        Console.Write('*');
+                    else
+                        Console.Write('-');
+                }
+                Console.WriteLine();
+            }
+        }
+        private static void Method2()
+        {
+          
+
+            for (int i = 0; i <= Height; i++)
+            {
+                for (int l = 0; l <= Height; l++)
+                {
+                    if (i == 0 || i == Height || l == 0 || l == Height || i == l || l == Height)
+
+                        Console.Write('*');
+                    else
+                        Console.Write('-');
+                }
+                Console.WriteLine();
+            }
+        }
+        private static void Method3()
+        {
+            
+
+            for (int i = 0; i <= Height; i++)
+            {
+                for (int l = 0; l <= Height; l++)
+                {
+                    if (i == 0 || i == Height || l == 0 || l == Height || i == l || l == (Height - i))
+
+                        Console.Write('*');
+                    else
+                        Console.Write('-');
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
